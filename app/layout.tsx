3dd82178b407bee60f81644bda5d41e778ce57ff
Wiem Mimouni <wiemmimouni74@gmail.com>
2023-09-13 14:16:34 +0100

    loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Script from 'next/script'
 import { Suspense } from 'react'
-//import Loading from './Loading'
+import Loading from './Loading'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
