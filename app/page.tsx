b13935378754629d2612ad212b9a2787da9d90f7
Wiem Mimouni <wiemmimouni74@gmail.com>
2023-09-13 21:23:33 +0100

    page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import Featured from "./components/home/Featured";
+import MainBanner from "./components/home/MainBanner";
 
 export default function Home() {
   return (
-    <div > 
-
-    </div>
-  )
+    <main>
+      <MainBanner />
+      <Featured/>
+    </main>
+  );
 }
