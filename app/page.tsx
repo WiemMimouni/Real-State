b50c0144544620195610007fa2be3482a6102e67
Wiem Mimouni <wiemmimouni74@gmail.com>
2023-09-15 10:11:09 +0100

    pagetsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
+import Achivements from "./components/home/achievements";
+import BestDeals from "./components/properties/BestDeal";
+import Contact from "./components/home/contact";
 import Featured from "./components/home/Featured";
 import MainBanner from "./components/home/MainBanner";
+import Properties from "./components/home/Properties";
+import VideoSection from "./components/home/videosection";
 
 export default function Home() {
   return (
     <main>
       <MainBanner />
       <Featured/>
+      <VideoSection/>
+      <Achivements/>
+      <BestDeals/>
+      <Properties/>
+      <Contact/>
     </main>
   );
 }
